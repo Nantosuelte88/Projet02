c0d1465f5e733197169dfc274e28297be7775112
Deborah Druine <deborahdruine@gmail.com>
2023-05-26 14:01:35 +0200

    url d'image extraite mais le 'g' de jpg à disparu...

--- main.py
+++ main.py
@@ -13,16 +13,6 @@ if reponse.ok:
     for td in tds:
         infos_tableau.append(td.string)
 
-    upc = infos_tableau[0]
-    price_exclud_tax = infos_tableau[2]
-    price_includ_tax = infos_tableau[3]
-    number_available = infos_tableau[5]
-    title = soup.h1.string
-
-#   reste à trouver l'url du produit et l'url de l'image :
-#    image_url = soup.find('div', class_="item active")
-#    print(image_url)
-
     descriptions = soup.find_all('p')
     description_livres = []
     for description in descriptions:
@@ -35,4 +25,39 @@ if reponse.ok:
         links.append(categorie.string)
     product_category = links[3]
 
-    print(upc, price_exclud_tax, price_includ_tax, number_available, title, product_category)
+    title = soup.h1.string
+
+    images = soup.find_all("img")
+    image_url = []
+    for image in images:
+        image_url.append(image)
+    url_image = str(image_url[0])
+ #   test_rep = url_image.replace('<img alt="' + title + '" src="', "")
+    test_strip = url_image.strip("<>img alt =\"/ src" + title)
+    print(test_strip)
+
+    dico_livres = {
+        "product_page_url" : "En cours...",
+        "upc": infos_tableau[0],
+        "title": soup.h1.string,
+        "price_including_tax": infos_tableau[3],
+        "price_excluding_tax": infos_tableau[2],
+        "number_available": infos_tableau[5],
+        "product_description": description_livres[3],
+        "product_category": links[3],
+        "review_rating" : infos_tableau[6],
+#        "image_url" : image_url[0]
+    }
+#    print(dico_livres)
+ #   print(upc, " \n", price_exclud_tax, " \n", price_includ_tax, " \n", number_available, " \n", title, " \n", product_category, " \n", product_description)
+
+""""
+    upc = infos_tableau[0]
+    price_exclud_tax = infos_tableau[2]
+    price_includ_tax = infos_tableau[3]
+    number_available = infos_tableau[5]
+    title = soup.h1.string
+"""
+#   reste à trouver l'url du produit et l'url de l'image :
+#    image_url = soup.find('div', class_="item active")
+#    print(image_url)
