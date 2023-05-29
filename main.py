4037976c7873b4ee43bade8deab3f3a6ae63f545
Deborah Druine <deborahdruine@gmail.com>
2023-05-29 13:38:55 +0200

    utilisation des methodes replace() et strip() pour l'extraction de l'url de l'image, commit AVANT la mise en place du scrap d'une categorie entiere

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ reponse = requests.get(url)
 if reponse.ok:
     soup = BeautifulSoup(reponse.content, 'html.parser')
 
+#    next_element = soup.find()
+
+
+#   Début d'extraction d'infos pour un livre
     infos_tableau = []
     tds = soup.find_all('td')
     for td in tds:
@@ -32,9 +36,8 @@ if reponse.ok:
     for image in images:
         image_url.append(image)
     url_image = str(image_url[0])
- #   test_rep = url_image.replace('<img alt="' + title + '" src="', "")
-    test_strip = url_image.strip("<>img alt =\"/ src" + title)
-    print(test_strip)
+    test_rep = url_image.replace('img alt', "")
+    test_strip = test_rep.strip("<>=\"/ src" + title)
 
     dico_livres = {
         "product_page_url" : "En cours...",
@@ -46,18 +49,8 @@ if reponse.ok:
         "product_description": description_livres[3],
         "product_category": links[3],
         "review_rating" : infos_tableau[6],
-#        "image_url" : image_url[0]
+        "image_url" : test_strip
     }
-#    print(dico_livres)
- #   print(upc, " \n", price_exclud_tax, " \n", price_includ_tax, " \n", number_available, " \n", title, " \n", product_category, " \n", product_description)
+    print(dico_livres)
 
-""""
-    upc = infos_tableau[0]
-    price_exclud_tax = infos_tableau[2]
-    price_includ_tax = infos_tableau[3]
-    number_available = infos_tableau[5]
-    title = soup.h1.string
-"""
-#   reste à trouver l'url du produit et l'url de l'image :
-#    image_url = soup.find('div', class_="item active")
-#    print(image_url)
+#   Fin d'extraction
