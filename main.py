e8ef6c49fd14645160a24377fae6cd0b2f15a48a
Deborah Druine <deborahdruine@gmail.com>
2023-05-29 19:12:04 +0200

    mise en place boucle permettant d'aller dans chaque page de livre d'une categorie, sauvegarde avant verification du bouton next

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/catalogue/the-guilty-will-robie-4_750/index.html'
+url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
 
 reponse = requests.get(url)
 
@@ -12,45 +12,61 @@ if reponse.ok:
 
 
 #   Début d'extraction d'infos pour un livre
-    infos_tableau = []
-    tds = soup.find_all('td')
-    for td in tds:
-        infos_tableau.append(td.string)
+    bouquins = soup.find_all('article', class_='product_pod')
+    url_livres = []
+    for bouquin in bouquins:
+        a = bouquin.find('a')
+        url_livre = a['href']
+        url_propre = url_livre.replace("../../../", "")
+        url_livres.append('http://books.toscrape.com/catalogue/' + url_propre)
 
-    descriptions = soup.find_all('p')
-    description_livres = []
-    for description in descriptions:
-        description_livres.append(description.string)
-    product_description = description_livres[3]
+    print(len(url_livres))
 
-    categories = soup.find_all('a')
-    links = []
-    for categorie in categories:
-        links.append(categorie.string)
-    product_category = links[3]
+    for a in url_livres:
+        page = requests.get(a)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        if page.ok:
+            #   Début d'extraction d'infos pour un livre
+            infos_tableau = []
+            tds = soup.find_all('td')
+            for td in tds:
+                infos_tableau.append(td.string)
 
-    title = soup.h1.string
+            descriptions = soup.find_all('p')
+            description_livres = []
+            for description in descriptions:
+                description_livres.append(description.string)
+            product_description = description_livres[3]
 
-    images = soup.find_all("img")
-    image_url = []
-    for image in images:
-        image_url.append(image)
-    url_image = str(image_url[0])
-    test_rep = url_image.replace('img alt', "")
-    test_strip = test_rep.strip("<>=\"/ src" + title)
+            categories = soup.find_all('a')
+            links = []
+            for categorie in categories:
+                links.append(categorie.string)
+            product_category = links[3]
 
-    dico_livres = {
-        "product_page_url" : "En cours...",
-        "upc": infos_tableau[0],
-        "title": soup.h1.string,
-        "price_including_tax": infos_tableau[3],
-        "price_excluding_tax": infos_tableau[2],
-        "number_available": infos_tableau[5],
-        "product_description": description_livres[3],
-        "product_category": links[3],
-        "review_rating" : infos_tableau[6],
-        "image_url" : test_strip
-    }
-    print(dico_livres)
+            title = soup.h1.string
 
-#   Fin d'extraction
+            images = soup.find_all("img")
+            image_url = []
+            for image in images:
+                image_url.append(image)
+            url_image = str(image_url[0])
+            test_rep = url_image.replace('img alt', "")
+            test_strip = test_rep.strip("<>=\"/ src" + title)
+
+            dico_livres = {
+                "product_page_url": page,
+                "upc": infos_tableau[0],
+                "title": soup.h1.string,
+                "price_including_tax": infos_tableau[3],
+                "price_excluding_tax": infos_tableau[2],
+                "number_available": infos_tableau[5],
+                "product_description": description_livres[3],
+                "product_category": links[3],
+                "review_rating": infos_tableau[6],
+                "image_url": test_strip
+            }
+            #   Fin d'extraction du livre
+
+
+            print(dico_livres)
