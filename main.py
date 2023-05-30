c7b0fe563669b103603e704806f5df2f9bb91d5c
Deborah Druine <deborahdruine@gmail.com>
2023-05-30 17:17:38 +0200

    mise en place recherche bouton next + prendre urls des livres puis aller sur la page next.. marche actuellement pour 2 pages, pas plus.. Revoir la logique de boucle

--- main.py
+++ main.py
@@ -1,31 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
+url = 'http://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html'
 
 reponse = requests.get(url)
 
 if reponse.ok:
     soup = BeautifulSoup(reponse.content, 'html.parser')
 
+    #   verification bouton next
+    next_element = []
+    elements = soup.find_all('li', class_='next')
+    for element in elements:
+        a = element.find('a')
+        next_elt = a['href']
+        next_element.append(next_elt)
+    next_rep = url.replace("index.html", "")
+    next_url = next_rep + next_element[0]
+
 #   Début d'extraction d'infos pour un livre
     bouquins = soup.find_all('article', class_='product_pod')
     url_livres = []
+    i = 0
     for bouquin in bouquins:
         a = bouquin.find('a')
         url_livre = a['href']
         url_propre = url_livre.replace("../../../", "")
         url_livres.append('http://books.toscrape.com/catalogue/' + url_propre)
+        i += 1
+        print("Dans boucle bouquins", i)
+    len_url = len(url_livres)
+    j = 0
+    print("len avant boucle next", len(url_livres))
+    if next_url is not None:
+        next_page = requests.get(next_url)
+        soup = BeautifulSoup(next_page.content, 'html.parser')
+        if next_page.ok:
+            print("Next ok")
+            bouquins = soup.find_all('article', class_='product_pod')
+            for bouquin in bouquins:
+                a = bouquin.find('a')
+                url_livre = a['href']
+                url_propre = url_livre.replace("../../../", "")
+                url_livres.append('http://books.toscrape.com/catalogue/' + url_propre)
+                j += 1
+                print("boucle next", j)
 
-    print(len(url_livres))
-
+    print("len après les boucles", len(url_livres))
+    """
     for a in url_livres:
         page = requests.get(a)
         soup = BeautifulSoup(page.content, 'html.parser')
         if page.ok:
             title = soup.h1.string
             print(title)
-            """
+
             #   Début d'extraction d'infos pour un livre
             infos_tableau = []
             tds = soup.find_all('td')
@@ -65,23 +94,5 @@ if reponse.ok:
                 "image_url": test_strip
             }
             print(dico_livres)
-            """
+"""
             #   Fin d'extraction du livre
-
-    #   verification bouton next
-    next_element = []
-    elements = soup.find_all('li', class_='next')
-    for element in elements:
-        a = element.find('a')
-        next_elt = a['href']
-        next_element.append(next_elt)
-
-    next_rep = url.replace("index.html", "")
-    next_url = next_rep + next_element[0]
-    next_page = requests.get(next_url)
-    soup = BeautifulSoup(next_page.content, 'html.parser')
-    if next_page.ok:
-        print("Next ok")
-    print(next_url, "Next nope")
-
-    #   fin verification next
