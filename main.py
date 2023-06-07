d4210ab1e6643f7e5a04b7759c3dc76e372ff4e9
Deborah Druine <deborahdruine@gmail.com>
2023-06-07 13:30:34 +0200

    galere fichiers csv et trouver bon dossir pour enregistrer images.. test d'une nouvelle logique pour boucler et enregistrer le dictionnaire

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ url = 'https://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 reponse = requests.get(url)
 d = 1
 n = 1
-
+e = 0
 if reponse.ok:
     soup = BeautifulSoup(reponse.content, 'html.parser')
 
@@ -50,10 +50,12 @@ if reponse.ok:
             print("PAS DE NEXT", len(url_livres))
 
             break
-
+        b = 0
+        e += 1
     for a in url_livres:
         page = requests.get(a)
         soup = BeautifulSoup(page.content, 'html.parser')
+        b = + 1
         if page.ok:
             #   Début d'extraction d'infos pour un livre
             infos_tableau = []
@@ -103,24 +105,90 @@ if reponse.ok:
                 "review_rating": infos_tableau[6],
                 "image_url": clean_url_img
             }
-            # telecharger l'image
+            print(b, e, len(dico_livres), len(url_livres), "\n", dico_livres, "\n")
+            liste_dico.append([dico_livres])
+  #          print(liste_dico)
+            en_tetes = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax",
+                        "number_available", "product_description", "product_category", "review_rating", "image_url"]
+            nom_csv = str(links[3]) + ".csv"
+            # Creation fichier CSV
+
+
+
+
+            
+            testeuh = [
+                [{111: 'lfrjkedlk', 222 : 'iedsjutiue', 333 : 'kgtjtrekij', 444 : 'ooigdf'}],
+                [{'AAA': 'lfrjkedlk', 'BBB': 'iedsjutiue', 'CCC': 'kgtjtrekij', 'DDD': 'ooigdf'}],
+                [{555: 'lfrjkedlk', 666: 'iedsjutiue', 777: 'kgtjtrekij', 888: 'ooigdf'}]
+            ]
+            dico_de_dico = {"BLABLA" : 574, "BLOUBLOU" : 778}, {"BLABLA" : 566584, "BLOUBLOU" : 7876854}, {"BLABLA" : 54574, "BLOUBLOU" : 854778}
+            test_dico_entetes = ["BLABLA", "BLOUBLOU"]
+            liste_test_un = ['Georges Dupont', 'Luc Martin', 'Lucas Anderson', 'Alexandre Petit']
+            liste_test_deux = ['jghgjkjes Dupont', 'Ljghjhg hfghjgfhn', 'Lhg gfhgfas Ason', 'A hfggfhandre Petit']
+            comptes = {"Georges Dupont": 10000, "Luc Martin": 150, "Lucas Anderson": 300, "Alexandre Petit": 1800.74}
+
+            test_tuple = (1, 2, 3, 'a', 'b')
+
+            with open(nom_csv, 'w', encoding='utf-8') as fichier_csv:
+                writer = csv.DictWriter(fichier_csv, fieldnames=test_dico_entetes, delimiter=',')
+                writer.writeheader()
+                for liste in dico_de_dico:
+                    writer.writerow(liste)
+
+
+
+
+"""
+
+            with open('test.csv', 'w', encoding='utf-8') as fichier_csv:
+                writer = csv.writer(fichier_csv, delimiter=',')
+                for liste in zip(liste_dico):
+                    writer.writerow([liste])
+
+
+
+                        print(url_livres)
+            en_tetes = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax",
+                        "number_available", "product_description", "product_category", "review_rating", "image_url"]
+            nom_csv = str(links[3]) + ".csv"
+            with open(nom_csv, 'w', encoding='utf-8') as fichier_csv:
+                writer = csv.DictWriter(fichier_csv, fieldnames=en_tetes)
+                writer.writeheader()
+                for liste in zip(image_url):
+                    writer.writerows([liste])
+
+ #           print(liste_dico)
+
+
+
+
+            # telechargement de l'image
             name_url_img = url_image[-39:-3]
             name_img = name_url_img
             img_data = requests.get(clean_url_img).content
             with open(name_img, 'wb') as img_tel:
                 img_tel.write(img_data)
 
-    """            
+
+
+
+
+
+
+
+
+
+
                 for it in dico_livres.fromkeys('title'):
                     liste_dico.append(it)
                     print(it)
                 print("liste dico = ", liste_dico)
-    
-    
-    
-    
-                en_tetes = ["product_page_url", "upc", "title", "price_including_tax", "price_excluding_tax", "number_available", "product_description", "product_category", "review_rating", "image_url"]
-    
+
+
+
+
+
                 with open('test.csv', 'w', encoding='utf-8') as fichier_csv:
                     writer = csv.DictWriter(fichier_csv, fieldnames=en_tetes)
                     writer.writeheader()
